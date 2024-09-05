e611e291e8c525b5f678609d7b8e30e3d3fca99b
339719510 <339719510@2260I-L29-004>
2024-09-05 09:37:13 -0400

    Add project files.

---
+++ Topic 1 Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Topic_1_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("");
+            Console.WriteLine("Hello! My name is Bailey");
+            Console.WriteLine("");
+            Console.WriteLine("comments are used to communicate when coding without disturbing the project itself when running.");
+            Console.WriteLine("");
+            Console.WriteLine("BBBB  H   H");
+            Console.WriteLine("B   B H   H");
+            Console.WriteLine("B   B H   H");
+            Console.WriteLine("BBBB  HHHHH");
+            Console.WriteLine("B   B H   H");
+            Console.WriteLine("B   B H   H");
+            Console.WriteLine("BBBB  H   H");
+        }
+    }
+}
